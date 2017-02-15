c7918df6cf5ab3b16ecda52a0a9a9442bc2865fe
Extreeeme <martindcontact@gmail.com>
2017-02-15 10:11:06 +0100

    Les vérifs

--- register.php
+++ register.php
@@ -1,3 +1,23 @@
+<?php
+	$error = array();
+	if (!empty($_POST)){
+
+		if (empty($_POST['username']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['username'])){
+			$error['username'] = "Votre pseudo n'est pas valide (alpha_numerique)";
+		}
+		if (empty($_POST['mail']) || !filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL)){
+			$error['mail'] = "Votre mail n'est pas valide";
+		}
+		if (empty($_POST['password']) || $_POST["password"] != $_POST["password-confirm"]){
+			$error['password'] = "Les mot de passe ne correspondent pas";
+		}
+		var_dump($error);
+
+
+	}
+?>
+
+
 <?php require 'inc/header.php';?>
 
 <h1>S'inscrire</h1>
@@ -5,10 +25,22 @@
 <form action="" method="post">
 	
 	<div class="form-group">
-		
-		
-
+		<input type="text" name="username" class="form-control" placeholder="Pseudo" />
 	</div>	
+
+	<div class="form-group">
+		<input type="text" name="mail" class="form-control" placeholder="Votre adresse mail" />
+	</div>
+
+	<div class="form-group">	
+		<input type="password" name="password" class="form-control" placeholder="Mot de passe" />	
+	</div>
+
+	<div class="form-group">	
+		<input type="password" name="password-confirm" class="form-control" placeholder="Confirmation du mot de passe" />	
+	</div>
+
+	<button type="submit" class="btn btn-primary">Inscription</button>
 
 </form>
 
